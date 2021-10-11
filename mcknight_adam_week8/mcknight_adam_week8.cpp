fa6e69fab5a5f26aa99af2f0267dbd40ae0f6cfb
Adam McKnight <adammcknight@me.com>
2021-10-11 16:15:10 -0400

    Added Week 8's Assignment. Hello World, push to version control.

---
+++ mcknight_adam_week8/mcknight_adam_week8.cpp
@@ -0,0 +1,18 @@
+﻿/*
+	Adam McKnight
+	CIS 1202 500
+	October 24th, 2021
+
+	Expected Output: "Hello World!"
+
+	Project checked into version control, using git. Pushed to GitHub repo.
+*/
+#include "mcknight_adam_week8.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!." << endl;
+	return 0;
+}
