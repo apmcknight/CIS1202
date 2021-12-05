04cc60b7b6b01ecd80a3b637c5fd84ed7af5dddb
Adam McKnight <adammcknight@me.com>
2021-12-05 18:26:48 -0500

    Added initial project files. Created a main project file mcknight_adam_week15 for project a, and mcknight_adam_week15_b for project b. Wrote initial functions and prototypes. Added comments to files.

---
+++ mcknight_adam_week15/mcknight_adam_week15.cpp
@@ -0,0 +1,26 @@
+﻿// Adam McKnight
+// December 6th 2021
+// CIS 1202
+// See screenshots in the project root for expected output of program.
+
+#include "mcknight_adam_week15.h"
+
+using namespace std;
+
+char character(char start, int offset);
+
+char character(char start, int offset)
+{
+	char first_character;
+	start = first_character;
+	if (first_character != "A" )
+	{
+
+	}
+}
+
+int main()
+{
+	character("A", 1);
+	return 0;
+}
---
+++ mcknight_adam_week15/mcknight_adam_week15_b.cpp
@@ -0,0 +1,13 @@
+// Adam McKnight
+// December 6th 2021
+// CIS 1202
+// See screenshots in the project root for expected output of program.
+
+#include "mcknight_adam_week15.h"
+
+using namespace std;
+
+int main()
+{
+
+}
